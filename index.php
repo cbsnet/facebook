739e0a856eb41828e274db65e45b98e6df343b38
cbsnet <a.sale@cbsnet.it>
2016-10-03 13:38:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>CbsNet - Soluzioni Internet Versilia</title>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/default.css" media="screen">
+<!--[if IE 7]>
+        <link rel="stylesheet" type="text/css" href="css/ie7.css">
+<![endif]-->
+<!--[if IE 8]>
+        <link rel="stylesheet" type="text/css" href="css/ie8.css">
+<![endif]-->
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/cufon.js"></script>
+<script type="text/javascript" src="js/cufon_max.js"></script>
+<script type="text/javascript" src="js/3d_carousel.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+
+<script type="text/javascript">
+	window.fbAsyncInit = function() {
+		FB.Canvas.setSize();
+	}
+
+	function sizeChangeCallback() {
+		FB.Canvas.setSize();
+	
+	setTimeout(function(){ FB.Canvas.setSize(size_obj) },300);
+	}
+</script>
+<style type="text/css">
+<!--
+.home_text {
+	font-family: Verdana, Geneva, sans-serif;
+	color: #F90;
+	font-size: 11px;
+}
+-->
+</style>
+</head>
+
+<body style="overflow:hidden;">
+<!-- ***** HEADER START ***** -->
+
+<div id="header">
+<div class="header_wrapper">    
+    <!-- main menu -->
+    <div id="navigation">
+        <ul class="nav">
+        <li class="current" title="home-page"><a href="index.php"><span class="home_text">Home</span></a></li>
+        <li title="Quello che ti offriamo"><a href="services.html"><span>Servizi</span></a></li>
+        <li title="Alcuni siti realizzati da CbsNet"><a href="portfolio.html"><span>Portfolio</span></a></li>
+        <li title="Contatta CbsNet"><a href="contact.html"><span>Contatti</span></a></li> 
+        </ul> 
+    </div>
+    <!-- end of main menu -->
+</div><!-- End of header_wrapper -->
+</div><!-- End of header -->
+
+<!-- ***** HEADER END ***** -->
+        
+<div class="clear"></div>
+        
+<!-- *********** -->
+<!-- 3D Carousel -->
+<!-- *********** -->
+<div class="slider_bg">
+  <div class="slider_wrapper">
+        <div id="carousel">
+        	<div class="carousel-feature">
+        		<a href="#"><img class="carousel-image" alt="CbsNet - Soluzioni Internet" src="images/green_grey/3d_carousel/001.jpg"></a>
+        	</div>
+        	<div class="carousel-feature">
+        		<a href="#"><img class="carousel-image" alt="CbsNet - Soluzioni Internet" src="images/green_grey/3d_carousel/002.jpg"></a>
+        	</div>
+        	<div class="carousel-feature">
+        		<a href="#"><img class="carousel-image" alt="CbsNet - Soluzioni Internet" src="images/green_grey/3d_carousel/003.jpg"></a>
+        	</div>
+            <div class="carousel-feature">
+        		<a href="#"><img class="carousel-image" alt="CbsNet - Soluzioni Internet" src="images/green_grey/3d_carousel/004.jpg"></a>
+        	</div>
+            <div class="carousel-feature">
+        		<a href="#"><img class="carousel-image" alt="CbsNet - Soluzioni Internet" src="images/green_grey/3d_carousel/002.jpg"></a>
+        	</div>
+        </div><!-- End of carousel -->
+    <iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FCbsNet-Realizzazione-siti-web-Versilia%2F147320365332243%3Fsk%3Dapp_191031284272174&amp;layout=button_count&amp;show_faces=true&amp;width=100&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px; text-align: center;" allowTransparency="false"></iframe>
+    </div><!-- End of slider_wrapper -->
+</div><!-- End of slider_bg -->
+<span style="text-align: center"></span>
+
+<span style="text-align: center"></span>
+<!-- ****************** -->
+<!-- End of 3D Carousel -->
+<!-- ****************** -->
+
+<!-- ************ -->
+<!-- MAIN CONTENT -->
+<!-- ************ -->
+
+<div class="main_content_wrapper">
+    	<div id="teaser">
+        	<h2>Vuoi personalizzare la tua area su Facebook?</h2>
+            <p>Affidala a noi! Potrai distinguerti dagli altri con il tuo sito <br>
+              dalla grafica e i 
+              contenuti completamente personalizzati!
+            </p>
+        </div>
+        
+        <div class="boxes_bg">
+            <div class="box">
+                <h2>Siti aziendali</h2>
+                <p>Progettiamo  applicazioni internet...</p>
+                <a href="services.html#siti">Scopri di più</a>
+            </div><!-- End of box 1 -->
+          <div class="box">
+                <h2>E-commerce</h2>
+                <p>Il commercio elettronico  <br>
+          è una delle opportunità...</p>
+                <a href="services.html?#ecommerce">Scopri di più</a>
+            </div><!-- End of box 2 -->
+            <div class="box box_last">
+                <h2>Cataloghi</h2>
+                <p>Cataloghi semplici da usare, puoi gestirli ...</p>
+                <a href="services.html#cataloghi">Scopri di più</a>
+            </div><!-- End of box 3 -->
+        </div><!-- End of boxes_bg -->
+        
+        <div class="content_520">
+        	<h3>Benvenuto nella nostra area Facebook</h3>
+            <p>Vuoi creare anche tu un'area personalizzata per la tua azienda? CsbNet ti dà la possibilità di essere presente su Facebook  a modo tuo con una bella vetrina con grafica e contenuti personalizzati per distinguerti dagli altri e dare valore ai tuoi prodotti! <br>
+            Non esitare, contattaci subito e scopri tutti i nostri servizi!</p>
+</div><!-- End of content_520 -->
+    </div><!-- End of main_contetn_wrapper -->
+
+<!-- ******************* -->
+<!-- END OF MAIN CONTENT -->
+<!-- ******************* -->
+
+<div class="clear"></div>
+
+<!-- ****** -->
+<!-- FOOTER -->
+<!-- ****** -->
+<div class="footer_bg">
+	<div class="footer_wrapper">
+        <div class="footer_box">
+        	<h4>Resta in contatto!</h4>
+          <p>Condividi con i tuoi amici queste informazioni oppure segui gli aggiornamenti del nostro blog iscrivendoti al feed rss!</p>
+            <ul class="social">
+            	<li><a href="http://www.facebook.com/pages/CbsNet-Realizzazione-siti-web-Versilia/147320365332243?sk=app_191031284272174"><img src="images/green_grey/icons/ico_facebook.png" alt="Bacheca CbsNet!"></a></li>
+            	<li></li>
+                <li></li>
+                <li></li>
+                <li><a href="http://www.cbsnet.it/index.php/feed/" target="_blank"><img src="images/green_grey/icons/ico_rss.png" alt="feed rss"></a></li>
+            </ul>
+        </div><!-- End of footer_box 2 -->
+        <div class="footer_box box_last">
+        	<h4>Contatti<br>
+        	  <br>
+        	</h4>
+<div id="contact_page"><a href="contact.html" title="Richiedi maggiori informazioni!">modulo contatti</a></div>
+        </div><!-- End of footer_box 3 -->
+    </div><!-- End of footer wrapper -->
+</div><!-- End of footer_bg -->
+<!-- ************* -->
+<!-- END OF FOOTER -->
+<!-- ************* -->
+
+<div id="fb-root"></div>
+<script src="http://connect.facebook.net/en_US/all.js"></script>
+<script>
+	FB.init({
+		appId : '191031284272174',
+		status : true, // check login status
+		cookie : true, // enable cookies to allow the server to access the session
+		xfbml : true // parse XFBML
+	});
+</script>
+
+</body>
+</html>
